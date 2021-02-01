a1d0a7e2338a100ddb5ebbd6d634ce65e1cf6994
Lars Graubner <mail@larsgraubner.de>
2021-02-01 22:56:43 +0100

    add initial design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,85 @@
+import Image from 'next/image'
+import { Box, Flex, Heading, Container, Text, Link } from 'theme-ui'
+
 function Home() {
-  return 'hello world!'
+  return (
+    <Box>
+      <Flex
+        as="header"
+        sx={{
+          height: 62,
+          borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Image src="/logo.svg" alt="websitetalk.club" width={155} height={28} />
+      </Flex>
+      <Container as="main" mb={60}>
+        <Heading as="h1" px={3} pt={50} pb={55}>
+          Wir analysieren gemeinsam deine Website
+        </Heading>
+        <Flex sx={{ alignItems: 'center', justifyContent: 'center' }} mb={80}>
+          <Box mx={2} pt={10}>
+            <Image
+              src="/dan@2x.png"
+              alt="Daniel Menius"
+              width={100}
+              height={110}
+            />
+          </Box>
+          <Box mx={2}>
+            <Image
+              src="/lars@2x.png"
+              alt="Lars Graubner"
+              width={100}
+              height={100}
+            />
+          </Box>
+        </Flex>
+        <Heading as="h2" variant="subheadline" mb={24}>
+          Nächster Termin
+        </Heading>
+        <Box
+          p={20}
+          sx={{
+            borderRadius: 20,
+            backgroundColor: '#E6EBF0'
+          }}
+        >
+          <Heading as="h3" variant="club" mb={24}>
+            #3 Wir analysieren Deine Website (Design, UX, Code)
+          </Heading>
+          <Text variant="muted" mb={2}>
+            Datum
+          </Text>
+          <Text sx={{ fontSize: 3, lineHeight: 1 }} mb={24}>
+            03. Februar 2021
+          </Text>
+          <Text variant="muted" mb={2}>
+            Start
+          </Text>
+          <Text sx={{ fontSize: 3, lineHeight: 1 }} mb={24}>
+            19 Uhr
+          </Text>
+          <Link
+            sx={{
+              width: '100%',
+              variant: 'buttons.primary',
+              display: 'block',
+              textAlign: 'center',
+              textDecoration: 'none'
+            }}
+            target="_blank"
+            rel="noopener"
+            href="https://www.joinclubhouse.com/event/M1za48e2"
+          >
+            Zum Termin
+          </Link>
+        </Box>
+      </Container>
+    </Box>
+  )
 }
 
 export default Home
--- theme.ts
+++ theme.ts
@@ -1,34 +1,74 @@
 const theme = {
   breakpoints: ['40em', '52em', '64em'],
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  space: [0, 4, 8, 20, 32, 64, 128, 256, 512],
   fonts: {
     body:
-      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
-    heading: 'inherit',
+      'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'Poppins',
     monospace: 'Menlo, monospace'
   },
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontSizes: [12, 15, 18, 22, 24, 30, 48, 64, 96],
   fontWeights: {
     body: 400,
-    heading: 700,
-    bold: 700
+    heading: 800,
+    bold: 700,
+    medium: 500
   },
   lineHeights: {
     body: 1.5,
-    heading: 1.125
+    heading: 1.2,
+    subheadline: 1.15,
+    club: 1.333
   },
   colors: {
     text: '#000',
     background: '#fff',
-    primary: '#07c',
-    secondary: '#30c',
-    muted: '#f6f6f6'
+    white: 'white',
+    muted: 'rgba(0, 0, 0, 0.5)'
   },
   text: {
     heading: {
       fontFamily: 'heading',
       lineHeight: 'heading',
-      fontWeight: 'heading'
+      fontWeight: 'heading',
+      fontSize: 5,
+      textAlign: 'center'
+    },
+    subheadline: {
+      fontFamily: 'body',
+      fontWeight: 'bold',
+      fontSize: 2,
+      lineHeight: 'subheadline'
+    },
+    club: {
+      fontFamily: 'heading',
+      lineHeight: 'club',
+      fontWeight: 'heading',
+      fontSize: 4,
+      textAlign: 'left'
+    },
+    muted: {
+      color: 'muted',
+      fontSize: 1,
+      lineHeight: 1,
+      fontWeight: 'medium'
+    }
+  },
+  sizes: {
+    container: 390
+  },
+  buttons: {
+    primary: {
+      borderRadius: 8,
+      paddingY: '18px',
+      fontSize: 1,
+      lineHeight: 1,
+      fontWeight: 'medium',
+      color: 'white',
+      background: 'linear-gradient(45deg, #E92F85 0%, #FE4D00 100%)',
+      '&:active': {
+        transform: 'translate3d(0, 1px, 0)'
+      }
     }
   },
   styles: {
@@ -36,54 +76,6 @@ const theme = {
       fontFamily: 'body',
       lineHeight: 'body',
       fontWeight: 'body'
-    },
-    h1: {
-      variant: 'text.heading',
-      fontSize: 5
-    },
-    h2: {
-      variant: 'text.heading',
-      fontSize: 4
-    },
-    h3: {
-      variant: 'text.heading',
-      fontSize: 3
-    },
-    h4: {
-      variant: 'text.heading',
-      fontSize: 2
-    },
-    h5: {
-      variant: 'text.heading',
-      fontSize: 1
-    },
-    h6: {
-      variant: 'text.heading',
-      fontSize: 0
-    },
-    pre: {
-      fontFamily: 'monospace',
-      overflowX: 'auto',
-      code: {
-        color: 'inherit'
-      }
-    },
-    code: {
-      fontFamily: 'monospace',
-      fontSize: 'inherit'
-    },
-    table: {
-      width: '100%',
-      borderCollapse: 'separate',
-      borderSpacing: 0
-    },
-    th: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid'
-    },
-    td: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid'
     }
   }
 }
